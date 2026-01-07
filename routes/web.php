27bbba0816d942c2a5f72a5e4d0f76495908cf7b
unknown <algi>
2026-01-07 13:57:58 +0700

    eror dashboard web.php

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,11 @@ use App\Http\Controllers\ProductController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::middleware(['auth'])->group(function () {
     Route::resource('products', ProductController::class);
 });
